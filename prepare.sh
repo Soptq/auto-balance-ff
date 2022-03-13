590749578a13941d0ce7b2bb346ee30a125f5a37
Soptq <32592090+soptq@users.noreply.github.com>
2022-03-13 19:53:06 +0800

    fix: build commands

--- prepare.sh
+++ prepare.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
 # compile folks-finance-js-sdk
+sed -i '' "s|\"src/index.ts\"|\"src/**/*.ts\"|g" ./node_modules/folks-finance-js-sdk/tsconfig.json
 cd ./node_modules/folks-finance-js-sdk && tsc
 cd ../../
 # compile tinyman-ts-sdk
+sed -i '' "s|0xFFFFFFFFFFFFFFFFn|BigInt(0xFFFFFFFFFFFFFFFF)|g" node_modules/tinyman-ts-sdk/src/Transactions/Bootstrap.ts
+sed -i '' "s|es2021|es6|g" node_modules/tinyman-ts-sdk/tsconfig.json
+sed -i '' "s|esnext|commonjs|g" node_modules/tinyman-ts-sdk/tsconfig.json
+sed -i '' "s|\"type\": \"module\",||g" node_modules/tinyman-ts-sdk/package.json
+rm -rf ./node_modules/tinyman-ts-sdk/src/Examples
 cd ./node_modules/tinyman-ts-sdk && tsc
 cd ../../
-sed -i "s|'./src/asc.json'|'./node_modules/tinyman-ts-sdk/src/asc.json'|g" ./node_modules/tinyman-ts-sdk/dist/contracts.js
-sed -i "s|0xFFFFFFFFFFFFFFFFn|BigInt(0xFFFFFFFFFFFFFFFF);|g" node_modules/tinyman-ts-sdk/src/Transactions/Bootstrap.ts
+sed -i '' "s|'./src/asc.json'|'./node_modules/tinyman-ts-sdk/src/asc.json'|g" ./node_modules/tinyman-ts-sdk/dist/contracts.js
 
 # compile auto-balance
 tsc
 
 # modification
-sed -i "s|\"folks-finance-js-sdk/src/index\"|\"folks-finance-js-sdk/dist/index\"|g" ./dist/index.js
-sed -i "s|\"tinyman-ts-sdk/src/index\"|\"tinyman-ts-sdk/dist/index\"|g" ./dist/index.js
+sed -i '' "s|\"folks-finance-js-sdk/src/index\"|\"folks-finance-js-sdk/dist/index\"|g" ./dist/index.js
+sed -i '' "s|\"tinyman-ts-sdk\"|\"tinyman-ts-sdk/dist/index\"|g" ./dist/index.js
